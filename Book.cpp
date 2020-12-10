d48b56836bb3a350c84fdf8308613d8adcaffc2f
CMahk <68128075+CMahk@users.noreply.github.com>
2020-12-09 21:12:18 -0800

    Added checks to make sure that the ISBN13 is exactly 13 digits

--- Book.cpp
+++ Book.cpp
@@ -5,7 +5,7 @@ Book::Book() : Media() {
 	this->setAuthor("None");
 	this->setDate("00-00-0000");
 	this->setPages(0);
-	this->setISBN13(-1);
+	this->setISBN13(0);
 	this->setAvailable(false);
 }
 
@@ -31,7 +31,7 @@ Book::Book(Book* book) : Book(book->getTitle(), book->getAuthor(), book->getDate
 Book::Book(string title) : Media(title) {
 	this->setAuthor("None");
 	this->setDate("00-00-0000");
-	this->setISBN13(-1);
+	this->setISBN13(0);
 	this->setAvailable(false);
 }
 
@@ -68,7 +68,25 @@ int Book::getPages() {
 }
 
 void Book::setISBN13(uint64_t isbn13) {
-	this->isbn13 = isbn13;
+	// Invalid value right off the bat
+	if (isbn13 <= 0)
+		this->isbn13 = 0;
+	else
+	{
+		// Ensure that the value is exactly 13 digits
+		uint64_t copy = isbn13;
+		int digits = 0;
+		while (copy)
+		{
+			copy /= 10;
+			digits++;
+		}
+
+		if (digits != 13)
+			this->isbn13 = 0;
+		else
+			this->isbn13 = isbn13;
+	}
 }
 
 uint64_t Book::getISBN13() {
