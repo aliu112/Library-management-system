9a44145e5d5d530c24f2c4930366c32cac12402a
CMahk <68128075+CMahk@users.noreply.github.com>
2020-12-09 19:39:33 -0800

    Added another constructor for Book class

--- Book.h
+++ Book.h
@@ -14,6 +14,7 @@ public:
 	Book();
 	Book(string title);
 	Book(string title, string author, string datePublished, int numPages, uint64_t isbn13);
+	Book(string title, string author, string datePublished, int numPages, uint64_t isbn13, bool availability);
 	Book(Book* book);
 
 	// Populate virtual function
